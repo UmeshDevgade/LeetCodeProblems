3f2f7efc26a3a8d87aa04eeaed3b2295ae3c943c
Umesh Devgade <41591829+UmeshDevgade@users.noreply.github.com>
2026-09-09 08:50:10 +0530

    Count Commas in Range II - 2026-09-09T03:20:09.772Z

--- 3871-count-commas-in-range-ii/solution.java
+++ 3871-count-commas-in-range-ii/solution.java
@@ -1,29 +1,43 @@
-// 1 ms | 42.7 MB
+// 1 ms | 42.5 MB
 class Solution {
     public long countCommas(long n) {
-        int count = 0;
-        long cur = n;
-        while (cur > 0) {
-            count++;
-            cur = cur / 10;
+        long ans = 0;
+        long start = 1_000L;
+        long commas = 1;
+
+        while (start <= n) {
+            long end = Math.min(n, start * 1000L - 1);
+
+            ans += (end - start + 1) * commas;
+
+            start *= 1000L;
+            commas++;
         }
-        long sum = 0;
-        if(count < 4) sum = 0;
-        else if (count < 7) {
-            sum = (n-999);
-        }
-        else if (count < 10 ) {
-            sum = (n - 999999L)*2 +  (long)(999999L-999L);
-        }
-        else if (count < 13){
-            sum = (n - 999999999L)*3+(999999999L - 999999L)*2 + (long) (999999L-999L);
-        }
-        else if(count < 16) {
-            sum = (n - 999999999999L)*4+ (999999999999L - 999999999L)*3+(999999999L - 999999L)*2 +  (long)(999999L-999L);
-        }
-        else {
-                        sum = (n - 999999999999999L)*5+( 999999999999999L- 999999999999L)*4+ (999999999999L - 999999999L)*3+(999999999L - 999999L)*2 +  (long)(999999L-999L);
-        }
-        return sum;
+
+        return ans;
+        // int count = 0;
+        // long cur = n;
+        // while (cur > 0) {
+        //     count++;
+        //     cur = cur / 10;
+        // }
+        // long sum = 0;
+        // if(count < 4) sum = 0;
+        // else if (count < 7) {
+        //     sum = (n-999);
+        // }
+        // else if (count < 10 ) {
+        //     sum = (n - 999999L)*2 +  (long)(999999L-999L);
+        // }
+        // else if (count < 13){
+        //     sum = (n - 999999999L)*3+(999999999L - 999999L)*2 + (long) (999999L-999L);
+        // }
+        // else if(count < 16) {
+        //     sum = (n - 999999999999L)*4+ (999999999999L - 999999999L)*3+(999999999L - 999999L)*2 +  (long)(999999L-999L);
+        // }
+        // else {
+        //                 sum = (n - 999999999999999L)*5+( 999999999999999L- 999999999999L)*4+ (999999999999L - 999999999L)*3+(999999999L - 999999L)*2 +  (long)(999999L-999L);
+        // }
+        // return sum;
     }
 }
