52a7554ba8073e527c4761d7e0c1fb070a87b8eb
Umesh Devgade <41591829+UmeshDevgade@users.noreply.github.com>
2026-09-12 06:51:38 +0530

    Two Sum - 2026-09-12T01:21:36.943Z

--- 1-two-sum/solution.java
+++ 1-two-sum/solution.java
@@ -1,16 +1,19 @@
-// 50 ms | 45 MB
+// 38 ms | 47.1 MB
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] test = new int[2];
+                int[] arr= new int[2];
         for(int i=0;i<nums.length;i++){
-            //int sum=nums[i];
+            int sum=nums[i];
             for(int j=i+1;j<nums.length;j++){
-                if(target==(nums[i]+nums[j])){
-                    test[0]=i;
-                    test[1]=j;
+                sum=sum+nums[j];
+                if(sum==target){
+                    arr[0]=i;
+                    arr[1]=j;
+                    break;
                 }
+                sum=sum-nums[j];
             }
         }
-        return test;
+        return arr;
     }
 }
