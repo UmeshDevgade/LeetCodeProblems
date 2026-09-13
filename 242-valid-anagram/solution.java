1a221c4e1f710688823b2f599f2a1cd0a68ffbf4
Umesh Devgade <41591829+UmeshDevgade@users.noreply.github.com>
2026-09-13 18:05:16 +0530

    Valid Anagram - 2026-09-13

--- 242-valid-anagram/solution.java
+++ 242-valid-anagram/solution.java
@@ -1,17 +1,35 @@
-// 16 ms | 45 MB
+// 18 ms | 46.9 MB
 class Solution {
     public boolean isAnagram(String s, String t) {
-        
-        if(s.length()!=t.length()) return false;
-        Map<Character ,Integer> freqMap = new HashMap<>();
-        for (char c: s.toCharArray()) {
-            freqMap.put(c,(freqMap.getOrDefault(c,0)+1));
-        }
-        for (char c: t.toCharArray()) {
-            freqMap.put(c,(freqMap.getOrDefault(c,0)-1));
-            if(freqMap.get(c)<0) return false;
-        }
-        return true;
+                if (s.length() != t.length()) return false;
 
+        Map<Character, Integer> sfreq = new HashMap<>();
+        Map<Character, Integer> tfreq = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char sChar = s.charAt(i);
+            if (sfreq.containsKey(sChar)) {
+                sfreq.put(sChar, sfreq.get(sChar) + 1);
+            } else {
+                sfreq.put(sChar, 1);
+            }
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char tChar = t.charAt(i);
+            if (tfreq.containsKey(tChar)) {
+                tfreq.put(tChar, tfreq.get(tChar) + 1);
+            } else {
+                tfreq.put(tChar, 1);
+            }
+        }
+
+        for (char c : sfreq.keySet()) {
+            if (!tfreq.containsKey(c) || !tfreq.get(c).equals(sfreq.get(c))) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
